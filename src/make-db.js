ca5948a5819afc1e4e42e505b5b965f45f24dee7
Alex Kocharin <alex@kocharin.ru>
2014-08-17 11:14:37 +0400

    add info about people's packages

--- src/make-db.js
+++ src/make-db.js
@@ -8,17 +8,26 @@
 var request = require('request')
 var YAML = require('js-yaml')
 
-var data = {}
+var data = {
+  projects: {},
+  maintainers: {},
+}
+var maintainersDB = require('./maintainers.json')
+var npmUserToName = {}
+Object.keys(maintainersDB).forEach(function (maintainer) {
+  data.maintainers[maintainer] = {}
+  npmUserToName[maintainersDB[maintainer].npm] = maintainer
+})
 var projects = require('./projects.json').map(function (project) {
   if (typeof project === 'string') project = { name: project }
   project.npm = project.npm || project.name.toLowerCase()
   project.repo = project.repo || ('jshttp/' + project.npm)
-  data[project.name] = {}
+  data.projects[project.name] = {}
   return project
 })
 
 require('co')(function *() {
-  yield [getInfoFromNpm, getInfoFromGithub]
+  yield [getInfoFromNpm, getInfoFromGithub, getMaintainersInfo]
 
   // it's pretty-printed only for git diffs, don't edit that manually
   console.log(JSON.stringify(data, null, 2))
@@ -38,21 +47,38 @@ function *getTravis(repo) {
                                  + repo + '/master/.travis.yml'))
 }
 
+function *getOwnedPackages(user) {
+  var data = yield get('http://isaacs.iriscouch.com/'
+                     + 'registry/_design/app/_view/browseAuthors'
+                     + '?startkey=' + escapeJSON([user])
+                     + '&endkey=' + escapeJSON([user, {}])
+                     + '&group_level=1') // set group_level=2 for list
+  return JSON.parse(data).rows[0].value
+}
+
+function *getMaintainersInfo() {
+  var names = Object.keys(data.maintainers)
+  for (var i=0; i<names.length; i++) {
+    var mData = data.maintainers[names[i]]
+    mData.packages = yield getOwnedPackages(maintainersDB[names[i]].npm)
+  }
+}
+
 function *getInfoFromNpm() {
   for (var i=0; i<projects.length; i++) {
     var npm = yield getNpmInfo(projects[i].npm)
-    var name = projects[i].name
-    data[name].description = npm.description
-    data[name].npmUser =
-      npm.versions[npm['dist-tags'].latest]._npmUser.name
+    var projectData = data.projects[projects[i].name]
+    projectData.description = npm.description
+    projectData.maintainer =
+      npmUserToName[npm.versions[npm['dist-tags'].latest]._npmUser.name]
   }
 }
 
 function *getInfoFromGithub() {
   for (var i=0; i<projects.length; i++) {
     var travis = yield getTravis(projects[i].repo)
-    var name = projects[i].name
-    data[name].nodeVersion = travis.node_js.sort(versionSort)[0]
+    var projectData = data.projects[projects[i].name]
+    projectData.nodeVersion = travis.node_js.sort(versionSort)[0]
   }
 }
 
@@ -62,7 +88,7 @@ function *getInfoFromGithub() {
 
 function get(url) {
   return function(cb) {
-    console.error(' -> ' + url)
+    console.error(' -> ' + url.split('?')[0])
     request.get({
       url: url,
       encoding: 'utf8',
@@ -70,6 +96,11 @@ function get(url) {
       cb(err, body)
     })
   }
+}
+
+// for couchdb
+function escapeJSON(data) {
+  return encodeURIComponent(JSON.stringify(data))
 }
 
 function versionSort(a, b) {
